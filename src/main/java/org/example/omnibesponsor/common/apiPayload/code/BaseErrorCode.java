854c438334c7aa634d443f57afe47230ff586e25
Habeomsu <hbs5186@naver.com>
2025-04-12 17:53:46 +0900

    ApiPayload 구현

---
+++ src/main/java/org/example/omnibesponsor/common/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,6 @@
+package org.example.omnibesponsor.common.apiPayload.code;
+
+public interface BaseErrorCode {
+    public ErrorReasonDto getReason();
+    public ErrorReasonDto getReasonHttpStatus();
+}
