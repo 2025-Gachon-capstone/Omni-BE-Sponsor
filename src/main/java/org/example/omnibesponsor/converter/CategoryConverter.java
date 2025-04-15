5ec7d91554a4fe2ec02b4599aa90bbaf8fe8923f
Habeomsu <hbs5186@naver.com>
2025-04-15 11:13:01 +0900

    Category 가져오기 구현

--- src/main/java/org/example/omnibesponsor/converter/CategoryConverter.java
+++ src/main/java/org/example/omnibesponsor/converter/CategoryConverter.java
@@ -1,14 +1,34 @@
 package org.example.omnibesponsor.converter;
 
 import org.example.omnibesponsor.dto.CategoryReqDto;
+import org.example.omnibesponsor.dto.CategoryResDto;
 import org.example.omnibesponsor.entity.Category;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CategoryConverter {
 
+    // Dto -> Entity
     public static Category toCategory(CategoryReqDto.CreateCategory dto) {
         return Category.builder()
                 .title(dto.getTitle())
                 .build();
+    }
+
+
+    // Entity -> Dto
+    public static CategoryResDto.GetCategory toGetCategoryDto(Category category) {
+        return CategoryResDto.GetCategory.builder()
+                .categoryId(category.getCategoryId())
+                .title(category.getTitle())
+                .build();
+    }
+
+    public static List<CategoryResDto.GetCategory> toGetCategoryDto(List<Category> categories) {
+        return categories.stream()
+                .map(CategoryConverter::toGetCategoryDto)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/org/example/omnibesponsor/service/CategoryService.java
+++ src/main/java/org/example/omnibesponsor/service/CategoryService.java
@@ -1,10 +1,13 @@
 package org.example.omnibesponsor.service;
 
 import org.example.omnibesponsor.dto.CategoryReqDto;
+import org.example.omnibesponsor.dto.CategoryResDto;
 import org.example.omnibesponsor.entity.Category;
+
+import java.util.List;
 
 public interface CategoryService {
 
     Category createCategory(CategoryReqDto.CreateCategory createCategoryDto);
-
+    List<CategoryResDto.GetCategory> getCategories();
 }
