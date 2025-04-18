42cee25d62b3a8bf444e744210f257a3694f8c22
Habeomsu <hbs5186@naver.com>
2025-04-18 10:08:02 +0900

    #13 valid 추가

--- src/main/java/org/example/omnibesponsor/controller/CategoryController.java
+++ src/main/java/org/example/omnibesponsor/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package org.example.omnibesponsor.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import org.example.omnibesponsor.common.apiPayload.ApiResult;
 import org.example.omnibesponsor.dto.CategoryReqDto;
 import org.example.omnibesponsor.dto.CategoryResDto;
@@ -23,7 +24,7 @@ public class CategoryController {
 
     @PostMapping("/categories")
     @Operation(summary = "카테고리 생성 API",description = "카테고리 생성 API 입니다. 중복은 안됩니다.", tags = "Category")
-    public ApiResult<CategoryResDto.CreateCategory> createCategory(@RequestBody CategoryReqDto.CreateCategory createCategoryDto){
+    public ApiResult<CategoryResDto.CreateCategory> createCategory(@Valid @RequestBody CategoryReqDto.CreateCategory createCategoryDto){
 
         Category savedCategory = categoryService.createCategory(createCategoryDto);
         return ApiResult.onSuccess(new CategoryResDto.CreateCategory(savedCategory.getCategoryId(),savedCategory.getTitle()));
