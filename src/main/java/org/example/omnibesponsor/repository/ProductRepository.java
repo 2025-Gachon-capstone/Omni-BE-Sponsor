0e38d4b749d65d43e83af23ec6e80c53d060a971
Habeomsu <hbs5186@naver.com>
2025-06-02 15:46:48 +0900

    #55 N+1 문제 해결

--- src/main/java/org/example/omnibesponsor/repository/ProductRepository.java
+++ src/main/java/org/example/omnibesponsor/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package org.example.omnibesponsor.repository;
 import org.example.omnibesponsor.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,6 +11,13 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findBySponsor_Category_CategoryId(Long categoryId, Pageable pageable);
-    Optional<Product> findById(Long productId);
+
+    @EntityGraph(attributePaths = {"sponsor", "productCategory"})
     Page<Product> findByProductCategory_ProductCategoryId(Long productCategoryId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"sponsor", "productCategory"})
+    Page<Product> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"sponsor", "productCategory"})
+    Optional<Product> findById(Long productId);
 }
